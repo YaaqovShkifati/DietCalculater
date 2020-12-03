0a31035c9f879b0eb75f3832b86058819e166204
YaaqovShkifati <70407221+YaaqovShkifati@users.noreply.github.com>
2020-12-02 19:59:05 -0500

    Update DietCalculater2.java

--- DietCalculater2.java
+++ DietCalculater2.java
@@ -1,43 +1,4 @@
-/**
- * @auther Yaaqov Shkifati
- * @Since 02-20-2020
- * @verison 2.0 
- * Description: In this program, we are creating a calculator that
- * would measure how much the Basal Metabolic Rate(BMR) and the Total Energy
- * Expenditure(TEE). BMR is the total amount of calories that is burnt when a
- * person is asleep and the TEE is the total amount that will approximate
- * physical activity. Our objective in this program is to create a diet
- * calculator that would measure the BMR; by entering the gender, height in feet
- * and inches, age in years, weight in pounds. However, the BMR formula is in
- * measures of the metric- system, so would need first to write a method that
- * would convert weight from pounds to kilograms(kg) and other method from feet
- * and inches to centimeters (cm). Then will write another two methods that
- * would calculate the formulas for BMR and TEE. When writing method TEE we will
- * need to use if statements that would set the conditions of the person
- * activity levels. We are measuring the persons activity level by 3 sets; 1 is
- * light activity (BMR * 1.53), 2 is moderately active (BMR * 1.76), and 3
- * vigorously active (BMR * 2.25). The results would give us the total amount of
- * daily calories person burnt given the conditions of the BMR and TEE.
- *
- * I have chosen to use the trailer method to write this program. This method is
- * effective when you use the a method name while. While is a type of loop which
- * does repeatedly executes a target statement as long as a given condition is
- * true. I have chosen my condition if gender equals the character 'M' which
- * represents male or if character 'F' represents female or when gender does not
- * equal to character 'X' to exit the loop.
- *
- * The most difficult part when writing this program for me was choosing the
- * best way to structure this program. There are soo many different ways in fact
- * i wrote it in two different ways with both resulting the correct output. The
- * first version i had everything read and printed out in my main method inside
- * the while loop but, i realized that main was way too big. What i did in the
- * second version i just printed the out the results at the end of each method.
- * Which gave the same results it just looked a lot better with the main not
- * being soo compacted. For my personal opinion i prefer programs that are more
- * science related programs because not only my skills in programing would
- * improve but, also educates me on that topic.
- *
- */
+
 package dietcalculater2;
 
 import java.util.Scanner;
